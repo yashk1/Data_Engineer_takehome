a2705ac6f87ee571dd88c3f984de9f10da8d2021
Yash Sharma <Yash95kumar@gmail.com>
2022-10-28 11:36:39 -0700

    .

--- get_data_and_transform_script.py
+++ get_data_and_transform_script.py
@@ -3,7 +3,6 @@ import pandas as pd
 from pandas import json_normalize
 import inflection
 
-data = 
 
 with jsonlines.open('/Users/yash/github/Data_Engineer_takehome/ga_sessions_20160801_(2)_(1)_(1)_(1) copy.json', 'r') as jsonl_f:
      lst = [obj for obj in jsonl_f]
